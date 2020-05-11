f45db3d5e80071c2472db43581b1d61f0dd6ec99
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2020-05-11 15:03:01 -0300

    Add files via upload

--- NP01/cIndex.c
+++ NP01/cIndex.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define S 10e15
+#define S 10e8
 
 int main(int argc, char **argv){
     printf("Hello World!\n");
     double *f= calloc(S, sizeof(double));
     int i;
 
-    for (i= 0; i< argc; i++){
-        f[i]= atof(argv[i+1]);
+    for (i= 1; i< argc; i++){
+        f[i-1]= atof(argv[i]);
     };
 
-    for (i= 0; i< argc; i++){
+    for (i= 0; i< argc-1; i++){
         printf("%e\n", f[i]);
     };
 
