87d66dbca3d50df7bd4d4000459ffb7d539ba318
Gabriel-Marino <gcmarino404@gmail.com>
2022-08-06 15:33:31 -0300

    add to probability_wit_py the median calculator

--- probability_with_pi.js
+++ probability_with_pi.js
@@ -5,6 +5,8 @@
  * What's the median?
  * What's the average value?
  * What's the standard deviation?
+ * 
+ * Desmos Graph: https://www.desmos.com/calculator/ne5r1gcm7i
  */
  (
     function()
@@ -17,9 +19,14 @@
             const INDEX = COUNT.findIndex(key => key[0] === digit);
             INDEX === -1 ? COUNT.push([digit, 1]) :  COUNT[INDEX][1] += 1;
         });
-        COUNT.sort((a, b) => a[0] - b[0]).sort((a, b) => b[1] - a[1]);
+        COUNT.sort((a, b) => b[1] - a[1]);
         const MOST_LIKELY = COUNT[0];
-        const MEDIAN = 0;
+        COUNT.sort((a, b) => a[0] - b[0])
+        const MEDIAN = COUNT.map((digit, index, array) => {
+            const LEFT = array.slice(0, index+1).reduce((acc, num) => acc + num[1]/NUM_SAMPLES, 0);
+            const RIGHT = array.slice(index).reduce((acc, num) => acc + num[1]/NUM_SAMPLES, 0);
+            if (LEFT >= 1/2 && RIGHT >= 1/2) return digit[0];
+        }).filter(val => val);
         const       AVERAGE = COUNT.reduce((acc, num) => acc + (num[1]/NUM_SAMPLES)*Number(num[0]), 0);
         const SQUAREAVERAGE = COUNT.reduce((acc, num) => acc + (num[1]/NUM_SAMPLES)*Number(num[0])**2, 0);
         const STANDARD_DEVIATION = (SQUAREAVERAGE-AVERAGE**2)**(1/2);
