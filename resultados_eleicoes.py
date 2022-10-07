299207a2a370a3f35e93617919e269ca42d6830c
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2022-10-07 12:09:10 -0300

    Update resultados_eleicoes algorithm
    add an automatic stop if 99% of the ballots are counted

--- resultados_eleicoes.py
+++ resultados_eleicoes.py
@@ -2,13 +2,13 @@
 #   Inspirado por:  https://twitter.com/BrennoSullivan/status/1576655828089974785
 
 from json import loads
-from os import system
+from os import system, getpid
 from pandas import DataFrame
 from requests import get
 from time import sleep, ctime
 
 while True:
-    json_data = loads(get("https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json").content)['cand']    #   Solicita os dados dos candidatos a presidência da API do TSE-BR.
+    json_data = loads(get("https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json").content)  #   Solicita os dados dos candidatos a presidência da API do TSE-BR.
 
     sep_data = [
         [[], 'nm'],     #   candidatos.
@@ -16,10 +16,10 @@ while True:
         [[], 'pvap']    #   porcentagem.
         ]
 
-    list(map(lambda data: list(map(lambda info: data[0].append(info[data[1]]) if info['seq'] in '1 2 3 4 5 6 7 8 9'.split(' ') else 0, json_data)), sep_data))
+    list(map(lambda data: list(map(lambda info: data[0].append(info[data[1]]) if info['seq'] in '1 2'.split(' ') else 0, json_data['cand'])), sep_data))
 
     system('clear')     #   limpa o console antes de imprimir dados novos.
     print(ctime())      #   Imprime a data e hora atual.
-    print(DataFrame(list(zip(sep_data[0][0], sep_data[1][0], sep_data[2][0])), columns=['Candidato', '# votos', 'Porcentagem']))    #   Cria a tabela para mostrar os dados mais organizadamente.
+    print(DataFrame(list(zip(sep_data[0][0], sep_data[1][0], sep_data[2][0])), columns=['Candidato', '# votos', 'Porcentagem']))        #   Cria a tabela para mostrar os dados mais organizadamente.
 
-    sleep(300)      #   Espera por 5 minutos antes de imprimir os dados novos.
+    system('kill -9 {}'.format(getpid())) if float(json_data[''].replace(',', '.')) >= 99 else sleep(300)    #   Para o laço caso tenha apurado 99% das urnas, caso contrário espera por 5 minutos antes de imprimir os dados novos.
