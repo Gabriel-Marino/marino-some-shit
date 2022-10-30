7af800bbd07eee2a87d3b100c11a1d9967abbf39
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2022-10-30 17:17:22 -0300

    Add files via upload

--- resultados_eleicoes.py
+++ resultados_eleicoes.py
@@ -8,8 +8,8 @@ from requests import get
 from time import sleep, ctime
 
 while True:
-    json_data = loads(get("https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json").content)  #   Solicita os dados dos candidatos a presidência da API do TSE-BR.
-
+    json_data = loads(get("https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json").content)  #   Solicita os dados dos candidatos a presidência da API do TSE-BR.
+    poll_percentage = float(json_data['pvvc'].replace(',', '.'))/100
     sep_data = [
         [[], 'nm'],     #   candidatos.
         [[], 'vap'],    #   número de votos.
@@ -20,6 +20,7 @@ while True:
 
     system('clear')     #   limpa o console antes de imprimir dados novos.
     print(ctime())      #   Imprime a data e hora atual.
+    print("Foram apuradas {}%% urnas.".format(poll_percentage))
     print(DataFrame(list(zip(sep_data[0][0], sep_data[1][0], sep_data[2][0])), columns=['Candidato', '# votos', 'Porcentagem']))        #   Cria a tabela para mostrar os dados mais organizadamente.
 
-    system('kill -9 {}'.format(getpid())) if float(json_data[''].replace(',', '.')) >= 99 else sleep(300)    #   Para o laço caso tenha apurado 99% das urnas, caso contrário espera por 5 minutos antes de imprimir os dados novos.
+    system('kill -9 {}'.format(getpid())) if poll_percentage >= 99 else sleep(60)    #   Para o laço caso tenha apurado 99% das urnas, caso contrário espera por 5 minutos antes de imprimir os dados novos.
