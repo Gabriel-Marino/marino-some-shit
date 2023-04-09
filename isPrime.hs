79f9b1f0c77b0913803ce8c30ab1d1adfacb56e8
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2023-04-09 16:37:40 -0300

    Add files via upload

--- isPrime.hs
+++ isPrime.hs
@@ -1,9 +1,12 @@
 {-
     Test if a giver number is a Prime or not.
 -}
-isPrime :: Integer -> String
-isPrime number = "\n The number: " ++ show number ++ " is a Prime number? " ++ show result ++ "."
+isPrimeS :: Integer -> String
+isPrimeS number = "\n The number: " ++ show number ++ " is a Prime number? " ++ show result ++ "."
     where result = if number < 2 || number `mod` 2 == 0 && number > 2 then False else length (filter (\denominator -> number `mod` denominator == 0) [2..number `div` 2]) < 2
+
+isPrimeB :: Integer -> Bool
+isPrimeB number = if number < 2 || number `mod` 2 == 0 && number > 2 then False else length (filter (\denominator -> number `mod` denominator == 0) [2..number `div` 2]) < 2
 
 {-
     This one was made by ChatGPT
@@ -12,3 +15,9 @@ isPrimeGPT :: Integer -> Bool
 isPrimeGPT number
   | number < 2 || (number `mod` 2 == 0 && number > 2) = False
   | otherwise = length (filter (\denominator -> number `mod` denominator == 0) [1..(number `div` 2)]) < 2
+
+
+{-
+    mapM_ print(map isPrimeS [1..1000])
+    mapM_ print(filter isPrimeB [1..1000])
+-}
