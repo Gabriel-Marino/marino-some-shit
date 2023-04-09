d9fdfec48a940d21cae7de326457d07235b486d0
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2023-04-09 16:32:21 -0300

    Add files via upload

--- isPrime.hs
+++ isPrime.hs
@@ -1,16 +1,14 @@
 {-
     Test if a giver number is a Prime or not.
-    I even do not know if this work, but at least I tried.
 -}
-isPrime :: Integer -> Bool
-isPrime number = "The number:" ++ show number ++ "is a Prime number? " ++ show result ++ "."
-    where result = if number < 2 || number `mod` 2 == 0 && number > 2 then False else length (filter (\denominator -> number `mod` denominator == 0) [2..number/2]) < 2
-
+isPrime :: Integer -> String
+isPrime number = "\n The number: " ++ show number ++ " is a Prime number? " ++ show result ++ "."
+    where result = if number < 2 || number `mod` 2 == 0 && number > 2 then False else length (filter (\denominator -> number `mod` denominator == 0) [2..number `div` 2]) < 2
 
 {-
     This one was made by ChatGPT
 -}
-IsPrime :: Int -> Bool
-IsPrime number
+isPrimeGPT :: Integer -> Bool
+isPrimeGPT number
   | number < 2 || (number `mod` 2 == 0 && number > 2) = False
   | otherwise = length (filter (\denominator -> number `mod` denominator == 0) [1..(number `div` 2)]) < 2
